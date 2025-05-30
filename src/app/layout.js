0dd036ab333346255ba857a02ef76d7c5dfe5e27
gaurav avhale <avhalegaurav07@gmail.com>
2025-05-30 19:49:39 +0530

    meta-data-update

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Gaurav Avhale | Fullstack Developer</title>
+        <title>Gaurav Avhale | Web Developer Portfolio</title>
         <link rel="icon" href="images/svg.jpg"></link>
+        <meta name="keywords" content="Gaurav Avhale, web developer portfolio, front-end developer, back-end developer, full-stack developer, web development, software developer, JavaScript, React, HTML, CSS, Node.js, portfolio projects, hire developer"></meta>
+        <meta name="description" content="Explore Gaurav Avhale's web developer portfolio showcasing innovative projects in JavaScript, React, and full-stack development. Hire me for your next project!"></meta>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"/>
       </head>
       <body>
